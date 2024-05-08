0766a126c966de5742add1640f3472509dadf9f6
TeerapongP <thirapongp7@gmail.com>
2024-05-08 23:25:27 +0700

    add code js for skillmetrix

---
+++ skillMetrixJs/shortUnicodeString.js
@@ -0,0 +1,11 @@
+let names = ['Jojo', 'banglee', 'Oreo', 'prayut', 'Meow'];
+names.sort();
+
+console.log(names);
+// output => [ 'Jojo', 'Meow', 'Oreo', 'banglee', 'prayut' ]
+
+'J'.charCodeAt() //74
+'M'.charCodeAt() //77
+'O'.charCodeAt() //79
+'b'.charCodeAt() //98 
+'p'.charCodeAt() //112
